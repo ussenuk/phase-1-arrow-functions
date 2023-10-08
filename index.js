039a80e7773989b6512fef79a8b700a63f79f530
Ussen_Kimanuka <ukimanuka@gmail.com>
2023-10-08 22:50:56 +0300

    working with arrow functions

---
+++ index.js
@@ -0,0 +1,13 @@
+function divide (parameter1, parameter2){
+    return 2000 / 100;
+}
+
+divide();
+
+const square = parameter1 => parameter1 * parameter1;
+square ();
+
+let a = 2;
+let b = 3;
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add (a, b);
